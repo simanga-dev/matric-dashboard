719538aa7055ecfe98f468dfe3923ab8c2a78ad8
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-02-05 16:27:41 +0200

    feat(ui): update ui to be something at least I am lookin for

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import { Search } from "@/components/search"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -13,32 +14,43 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { UserNav } from "@/components/user-nav"
 
 export default function Page() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+
+        <header className="flex justify-center h-16 shrink-0 items-center gap-2 border-b px-4">
+
+          <div className="mx-auto">
+              <Search   />
           </div>
+
+          <div className="">
+              <UserNav />
+          </div>
+
+
+          {/* <SidebarTrigger className="-ml-1" /> */}
+          {/* <Separator orientation="vertical" className="mr-2 h-4" /> */}
+          {/* <Breadcrumb> */}
+          {/*   <BreadcrumbList> */}
+          {/*     <BreadcrumbItem className="hidden md:block"> */}
+          {/*       <BreadcrumbLink href="#"> */}
+          {/*         Building Your Application */}
+          {/*       </BreadcrumbLink> */}
+          {/*     </BreadcrumbItem> */}
+          {/*     <BreadcrumbSeparator className="hidden md:block" /> */}
+          {/*     <BreadcrumbItem> */}
+          {/*       <BreadcrumbPage>Data Fetching</BreadcrumbPage> */}
+          {/*     </BreadcrumbItem> */}
+          {/*   </BreadcrumbList> */}
+          {/* </Breadcrumb> */}
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+
+        <div className="flex flex-1 flex-col gap-4 p-4">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
