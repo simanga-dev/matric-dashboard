2f6ce6e5dde4fcadc8b681bd3fb2d26a293aeb6e
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-06-16 10:13:18 +0200

    refactor(access-layer): create data access layer

---
+++ src/app/dashboard/queries.ts
@@ -0,0 +1,159 @@
+import { db } from "~/server/db";
+import { z } from "zod";
+import { sql } from "drizzle-orm";
+
+const SchoolCardSchema = z.object({
+  total_school_2023: z.number(),
+  total_school_2022: z.number(),
+  trend_schools_2023: z.number(),
+  trend_percentage_2023: z.number().nullable(),
+});
+
+const LearnerCardSchema = z.object({
+  total_learners_2023: z.number(),
+  total_learners_2022: z.number(),
+  trend_learners_2023: z.number(),
+  trend_percentage_2023: z.number().nullable(),
+});
+
+type SchoolTrends = z.infer<typeof SchoolCardSchema>;
+type LearnerTrends = z.infer<typeof LearnerCardSchema>;
+
+const Q = {
+  GetMatricPassRate: async function (): Promise<LearnerTrends | null> {
+    try {
+      const result = await db.run(sql`
+        SELECT
+            ROUND(
+                ((SUM(CASE WHEN year = 2023 THEN total_archieved ELSE 0 END) * 1.0) /
+                 NULLIF(SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END), 0)) * 100, 2
+              ) AS total_learners_2023,
+            ROUND(
+                ((SUM(CASE WHEN year = 2022 THEN total_archieved ELSE 0 END) * 1.0) /
+                 NULLIF(SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END), 0)) * 100, 2
+              ) AS total_learners_2022,
+            ROUND(
+                (
+                    ((SUM(CASE WHEN year = 2023 THEN total_archieved ELSE 0 END) * 1.0) /
+                     NULLIF(SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END), 0)) -
+                    ((SUM(CASE WHEN year = 2022 THEN total_archieved ELSE 0 END) * 1.0) /
+                     NULLIF(SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END), 0))
+                ), 2
+              ) AS trend_learners_2023,
+
+            ROUND(
+                (
+                    ((SUM(CASE WHEN year = 2023 THEN total_archieved ELSE 0 END) * 1.0) /
+                     NULLIF(SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END), 0)) -
+                    ((SUM(CASE WHEN year = 2022 THEN total_archieved ELSE 0 END) * 1.0) /
+                     NULLIF(SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END), 0))
+                ) * 100, 2
+              ) AS trend_percentage_2023
+
+        FROM "matric-dashboard_marks"
+        WHERE year IN (2022, 2023)
+    `);
+
+      const row = result.rows?.[0];
+      const parsed = LearnerCardSchema.safeParse(row);
+
+      return parsed.success ? parsed.data : null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+
+  GetTotalLearner: async function (): Promise<LearnerTrends | null> {
+    try {
+      const result = await db.run(sql`
+        SELECT
+            total_learners_2023,
+            total_learners_2022,
+            trend_learners_2023,
+            CASE
+                WHEN total_learners_2022 = 0 THEN NULL
+                ELSE ROUND(100.0 * trend_learners_2023 / total_learners_2022, 2)
+            END AS trend_percentage_2023
+        FROM (
+            SELECT
+                SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END) AS total_learners_2023,
+                SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END) AS total_learners_2022,
+                SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END) -
+                SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END) AS trend_learners_2023
+            FROM "matric-dashboard_marks"
+            WHERE year IN (2022, 2023)
+        ) t
+`);
+
+      const row = result.rows?.[0];
+      const parsed = LearnerCardSchema.safeParse(row);
+      return parsed.success ? parsed.data : null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+
+  GetTopSchool: async function (): Promise<SchoolTrends | null> {
+    try {
+      const result = await db.run(sql`
+      SELECT
+          total_school_2023,
+          total_school_2022,
+          total_school_2023 - total_school_2022 AS trend_schools_2023,
+          CASE
+              WHEN total_school_2022 = 0 THEN NULL
+              ELSE ROUND(100.0 * (total_school_2023 - total_school_2022) / total_school_2022, 2)
+          END AS trend_percentage_2023
+      FROM (
+          SELECT
+              COUNT(DISTINCT CASE WHEN year = 2023 AND total_wrote = total_archieved THEN school_id END) AS total_school_2023,
+              COUNT(DISTINCT CASE WHEN year = 2022 AND total_wrote = total_archieved THEN school_id END) AS total_school_2022
+          FROM "matric-dashboard_marks"
+          WHERE year IN (2022, 2023)
+      ) t
+    `);
+
+      const row = result.rows?.[0];
+      const parsed = SchoolCardSchema.safeParse(row);
+      return parsed.success ? parsed.data : null;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  },
+
+  Examcenters: async function (): Promise<SchoolTrends | null> {
+    try {
+      const result = await db.run(sql`
+        SELECT
+            total_school_2023,
+            total_school_2022,
+            total_school_2023 - total_school_2022 AS trend_schools_2023,
+            CASE
+                WHEN total_school_2022 = 0 THEN NULL
+                ELSE ROUND(100.0 * total_school_2023 / total_school_2022, 2)
+            END AS trend_percentage_2023
+        FROM (
+            SELECT
+                COUNT(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END) AS total_school_2023,
+                COUNT(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END) AS total_school_2022,
+                COUNT(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END) -
+                COUNT(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END) AS total_school_2023
+            FROM "matric-dashboard_marks"
+            WHERE year IN (2022, 2023)
+        ) t
+      `);
+
+      const row = result.rows?.[0];
+      const parsed = SchoolCardSchema.safeParse(row);
+      return parsed.success ? parsed.data : null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+};
+
+export default Q;
--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -4,7 +4,7 @@ import {
   IconArrowNarrowRight,
 } from "@tabler/icons-react";
 
-import { sql } from "drizzle-orm";
+import Q from "~/app/dashboard/queries";
 
 import { Badge } from "~/components/ui/badge";
 import {
@@ -15,7 +15,6 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
-import { db } from "~/server/db";
 
 export function SectionCards() {
   return (
@@ -29,40 +28,20 @@ export function SectionCards() {
 }
 
 async function TopSchoolCard() {
-  const result = await db.run(sql`
-      SELECT
-          total_school_2023,
-          total_school_2022,
-          total_school_2023 - total_school_2022 AS trend_schools_2023,
-          CASE
-              WHEN total_school_2022 = 0 THEN NULL
-              ELSE ROUND(100.0 * (total_school_2023 - total_school_2022) / total_school_2022, 2)
-          END AS trend_percentage_2023
-      FROM (
-          SELECT
-              COUNT(DISTINCT CASE WHEN year = 2023 AND total_wrote = total_archieved THEN school_id END) AS total_school_2023,
-              COUNT(DISTINCT CASE WHEN year = 2022 AND total_wrote = total_archieved THEN school_id END) AS total_school_2022
-          FROM "matric-dashboard_marks"
-          WHERE year IN (2022, 2023)
-      ) t
-
-`);
+  const r = await Q.GetTopSchool();
+  if (r == null) return <Card className="@container/card"></Card>;
 
   return (
     <Card className="@container/card">
       <CardHeader>
         <CardDescription>Top School Performer</CardDescription>
         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-          {result.rows && result.rows[0]
-            ? `${result.rows[0].total_school_2023}`
-            : null}
+          {r.total_school_2023}
         </CardTitle>
         <CardAction>
           <Badge variant="outline">
             <IconTrendingDown />
-            {result.rows && result.rows[0]
-              ? ` ${result.rows[0].trend_percentage_2023} %`
-              : null}
+            {r.trend_percentage_2023} %
           </Badge>
         </CardAction>
       </CardHeader>
@@ -79,41 +58,20 @@ async function TopSchoolCard() {
 }
 
 async function ExamCentreCard() {
-  const result = await db.run(sql`
-        SELECT
-            total_student_2023,
-            total_student_2022,
-            trend_learners_2023,
-            CASE
-                WHEN total_student_2022 = 0 THEN NULL
-                ELSE ROUND(100.0 * trend_learners_2023 / total_student_2022, 2)
-            END AS trend_percentage_2023
-        FROM (
-            SELECT
-                COUNT(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END) AS total_student_2023,
-                COUNT(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END) AS total_student_2022,
-                COUNT(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END) -
-                COUNT(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END) AS trend_learners_2023
-            FROM "matric-dashboard_marks"
-            WHERE year IN (2022, 2023)
-        ) t
-`);
+  const r = await Q.Examcenters();
+  if (r == null) return <Card className="@container/card"></Card>;
 
   return (
     <Card className="@container/card">
       <CardHeader>
         <CardDescription>Exam Centers</CardDescription>
         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-          {result.rows && result.rows[0]
-            ? `${result.rows[0].total_student_2023}`
-            : null}
+          {r.total_school_2022}
         </CardTitle>
         <CardAction>
           <Badge variant="outline">
             <IconArrowNarrowRight />
-            {result.rows && result.rows[0]
-              ? `${result.rows[0].trend_percentage_2023} %`
-              : null}
+            {r.trend_percentage_2023}
           </Badge>
         </CardAction>
       </CardHeader>
@@ -130,50 +88,28 @@ async function ExamCentreCard() {
 }
 
 async function LearnersCountCard() {
-  const result = await db.run(sql`
-        SELECT
-            total_student_2023,
-            total_student_2022,
-            trend_learners_2023,
-            CASE
-                WHEN total_student_2022 = 0 THEN NULL
-                ELSE ROUND(100.0 * trend_learners_2023 / total_student_2022, 2)
-            END AS trend_percentage_2023
-        FROM (
-            SELECT
-                SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END) AS total_student_2023,
-                SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END) AS total_student_2022,
-                SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END) -
-                SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END) AS trend_learners_2023
-            FROM "matric-dashboard_marks"
-            WHERE year IN (2022, 2023)
-        ) t
-`);
+  const r = await Q.GetTotalLearner();
+
+  if (r == null) return <Card className="@container/card"></Card>;
 
   return (
     <Card className="@container/card">
       <CardHeader>
         <CardDescription>Learners in 2023</CardDescription>
         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-          {result.rows && result.rows[0]
-            ? `${result.rows[0].total_student_2023}`
-            : null}
+          {r.total_learners_2023}
         </CardTitle>
         <CardAction>
           <Badge variant="outline">
             <IconTrendingDown />
-            {result.rows && result.rows[0]
-              ? `${result.rows[0].trend_percentage_2023} %`
-              : null}
+            {r.trend_percentage_2023}
           </Badge>
         </CardAction>
       </CardHeader>
       <CardFooter className="flex-col items-start gap-1.5 text-sm">
         <div className="line-clamp-1 flex gap-2 font-medium">
           Learners decreased by
-          {result.rows && result.rows[0]
-            ? ` ${result.rows[0].trend_learners_2023}`
-            : null}
+          {r.trend_learners_2023}
           <IconTrendingDown className="size-4" />
         </div>
         <div className="text-muted-foreground">
@@ -185,43 +121,19 @@ async function LearnersCountCard() {
 }
 
 async function PassRateCard() {
-  const result = await db.run(sql`
-        SELECT
-            ROUND(
-                ((SUM(CASE WHEN year = 2023 THEN total_archieved ELSE 0 END) * 1.0) /
-                 NULLIF(SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END), 0)) * 100, 2
-              ) AS pass_rate_2023,
-            ROUND(
-                ((SUM(CASE WHEN year = 2022 THEN total_archieved ELSE 0 END) * 1.0) /
-                 NULLIF(SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END), 0)) * 100, 2
-              ) AS pass_rate_2022,
-            ROUND(
-                (
-                    ((SUM(CASE WHEN year = 2023 THEN total_archieved ELSE 0 END) * 1.0) /
-                     NULLIF(SUM(CASE WHEN year = 2023 THEN total_wrote ELSE 0 END), 0)) -
-                    ((SUM(CASE WHEN year = 2022 THEN total_archieved ELSE 0 END) * 1.0) /
-                     NULLIF(SUM(CASE WHEN year = 2022 THEN total_wrote ELSE 0 END), 0))
-                ) * 100, 2
-              ) AS trend_percentage_points
-        FROM "matric-dashboard_marks"
-        WHERE year IN (2022, 2023)
-`);
-
+  const r = await Q.GetMatricPassRate();
+  if (r == null) return <Card className="@container/card"></Card>;
   return (
     <Card className="@container/card">
       <CardHeader>
         <CardDescription>Pass Rate 2023</CardDescription>
         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-          {result.rows && result.rows[0]
-            ? `${result.rows[0].pass_rate_2023} %`
-            : null}
+          {r.total_learners_2023} %
         </CardTitle>
         <CardAction>
           <Badge variant="outline">
             <IconTrendingUp />
-            {result.rows && result.rows[0]
-              ? `+${result.rows[0].trend_percentage_points} %`
-              : null}
+            {r.trend_percentage_2023} %`
           </Badge>
         </CardAction>
       </CardHeader>
