487b8cac49369c26f20fb9df6f55a984312a63a1
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-08-31 08:04:06 +0200

    wip

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -4,6 +4,39 @@ export const Route = createFileRoute('/dashboard/')({
   component: DashboardHome,
 })
 
-function DashboardHome() {
-  return <p>Hello World</p>
+import data from './data.json'
+import { SidebarInset, SidebarProvider } from '~/components/ui/sidebar'
+import { AppSidebar } from '~/components/app-sidebar'
+import { SiteHeader } from '~/components/site-header'
+import { SectionCards } from '~/components/section-cards'
+import { ChartAreaInteractive } from '~/components/chart-area-interactive'
+import { DataTable } from '~/components/data-table'
+
+export default function DashboardHome() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              <DataTable data={data} />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
